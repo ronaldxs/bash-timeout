001d6457796a1791ccef0512c2abf9c10ae42c21
Ronald Schmidt <ronaldxs@software-path.com>
2017-04-12 07:24:20 -0400

    bare bones start commit

---
+++ bin/timeout.bash
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+
+# timeout - bash implementation of gnu timeout
+# Copyright (C) Ronald Schmidt
+# GPL License should be included in source repository.
+
+list_process_tree ()
+{
+    local p="$1"
+    local monitor="$2" # recursive calls have no $2 - ok
+    local children
+    if [[ ${monitor:+1} ]] ; then
+        children=$(ps -o pid= --ppid "$p" | grep -v "\b$monitor\$")
+    else
+        children=$(ps -o pid= --ppid "$p")
+    fi
+
+    for pid in $children
+    do
+        list_process_tree "$pid"
+    done
+
+    echo "$p"
+}
+
+[[   -z $TIMEOUT_IGNORE_CMD         &&
+     -n $(type -p timeout 2>&1)
+]] || function timeout {
+    echo in bash timeout
+
+    local exit_code time_limit=$1 prog=$2 timeout_marker
+    shift 2
+    if [[ -z $TIMEOUT_NO_RC_124 ]]; then
+        timeout_marker=$(
+            mktemp -q ||
+            mktemp -q -t "$(basename "$0").XXXXXX"
+        )
+        if [[ -n ${timeout_marker:+1} ]]; then
+            trap 'if [[ -e $timeout_marker ]]; then rm "$timeout_marker"; fi' EXIT
+        fi
+    fi
+
+    exec 3>&2
+    exec 2>/dev/null # suppress report of child process death
+    (
+        exec 2>&3 # normal stderr for sub process
+        mainpid=$(sh -c 'echo $PPID')
+        (
+            sleep $time_limit
+            monitor_pid=$(sh -c 'echo $PPID')
+#            ps
+
+            # remove not reliable after kill - race condition
+            if [[ -n ${timeout_marker:+1} ]]; then
+                rm "$timeout_marker"
+            fi
+
+            kill $(list_process_tree $mainpid $monitor_pid)
+        ) &
+        watchdogpid=$!
+        ${prog} "$@"
+        exit_code=$?
+        kill $watchdogpid
+        exit $exit_code
+    )
+    exit_code=$?
+
+    exec 2>&3 # restore stderr
+    exec 3>&- # close dup stderr
+
+    if [[ -n ${timeout_marker:+1} ]]; then
+        if [[ -e $timeout_marker ]]; then
+            rm $timeout_marker
+        elif [[ $exit_code -ne 0 ]]; then
+            exit_code=124
+        fi
+    fi
+
+#    ps
+    return $exit_code
+}
+
---
+++ t/prelim.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+function f {
+    echo called f
+    echo griddle
+    echo waddle
+    echo puddle
+    echo "$@"
+    sleep 4
+}
+
+function timely {
+    local time_limit=5
+
+    if [[ -n $1 && -z ${1//[0-9]} ]] ; then
+        time_limit=$1
+        shift
+    fi
+
+    $(dirname "$0")/../bin/b_timeout $time_limit "$@"
+}
+
+export -f f
+timely 5 bash -c "f whistle bustle"
+echo $? after f
+timely 2 bash -c "f whistle bustle"
+echo $? after f
+
+sudo bash -c "echo 3 >/proc/sys/vm/drop_caches"
+echo before find
+timely 4 bash -c "find / '*.pl' >/dev/null"
+echo $? after find
+
+timely 5 ls -l
+echo $? after ls
+# dup parent stderr
+# redirect parent stderr to /dev/null
+# redirect child stderr to dup
+# redirect parent stderr to dup
+# close dup
